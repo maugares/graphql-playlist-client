84063de3143d5cac0cb9c6534d6658f5797bbc0c
maugares <mauro.garciaesteban@gmail.com>
2019-06-19 16:46:35 +0200

    Define the graphql query to getting all the books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,4 +1,15 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+
+// Define the graphql query to getting all the books
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`
 
 export default class BookList extends Component {
   render() {
